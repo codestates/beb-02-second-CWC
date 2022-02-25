97b1234945847c3f680793738e7dcd47e1ab56f4
yonacode <62411918+yonacode@users.noreply.github.com>
2022-02-25 18:45:53 +0900

    add mainpage, cwc page & nav styling
    test add mainpage, cwc page & nav styling

--- CWC_project/client/src/components/CWC.js
+++ CWC_project/client/src/components/CWC.js
@@ -8,7 +8,6 @@ function CWC() {
       <div className="cwc-text">Hello World,</div>
       <div className="cwc-paragrph">
         CWC is an incentive token-based community. Web2.0 is the second stage of development of the World Wide Web, characterized especially by the change from static web pages to dynamic or user-generated content and the growth of social media.
-        updated.
       </div>    
     </div>  
  
