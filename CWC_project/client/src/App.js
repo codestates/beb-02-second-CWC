b4c6dcff2465779a04bd4463a29bf255615a9ba5
yonacode <62411918+yonacode@users.noreply.github.com>
2022-02-25 18:20:09 +0900

    add hompage, cwc page & nav css styling
    add hompage, cwc page & nav css styling

--- CWC_project/client/src/App.js
+++ CWC_project/client/src/App.js
@@ -3,25 +3,33 @@ import Mypage from "./views/Mypage";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Board from "./components/Board";
+import Home from "./components/Home";
+import CWC from "./components/CWC";
 
 function App() {
   return (
-
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/home" exact>
+            <Home />
+          </Route>
           <Route path="/post">
             <Board />
           </Route>
           <Route path="/profile">
             <Mypage />
           </Route>
+          <Route path="/cwc">
+            <CWC />
+          </Route>
           <Route path="/sign-up"></Route>
         </Switch>
       </Router>
+      
     </>
+ 
 
   );
 }
---
+++ CWC_project/client/src/components/CWC.js
@@ -0,0 +1,21 @@
+import "./CWC.css";
+import Mypage from "../views/Mypage";
+
+function CWC() {
+    return (
+    <div>  
+             
+      <div className="cwc-text">Hello World,</div>
+      <div className="cwc-paragrph">
+        CWC is an incentive token-based community. Web2.0 is the second stage of development of the World Wide Web, characterized especially by the change from static web pages to dynamic or user-generated content and the growth of social media.
+      </div>    
+    </div>  
+ 
+
+  
+       
+    );
+  }
+  
+  export default CWC;
+  
