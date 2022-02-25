ddde4b2ec7200a5edd27033e94e4e3f1e94a38a4
djshinnn <tls980824@naver.com>
2022-02-25 17:34:50 +0900

    merge

--- CWC_project/client/src/App.js
+++ CWC_project/client/src/App.js
@@ -3,6 +3,8 @@ import Mypage from "./views/Mypage";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Board from "./components/Board";
+import Footer from "./components/Footer";
+import Create from "./components/Create";
 
 function App() {
   return (
@@ -11,10 +13,19 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact />
+          <Route path="/post">
+            <Board />
+          </Route>
+          <Route path="/profile">
+            <Mypage />
+          </Route>
+          <Route path="/sign-up"></Route>
+          <Route path="/create">
+            <Create />
+          </Route>
         </Switch>
+        <Footer />
       </Router>
-      <Mypage />
-      <Board />
     </>
   );
 }
