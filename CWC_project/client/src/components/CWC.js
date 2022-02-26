3cd0509cbc92478bfad83d642a63b06f87e6085e
yonacode <62411918+yonacode@users.noreply.github.com>
2022-02-26 17:19:29 +0900

    hompage post
    homepage post

--- CWC_project/client/src/components/CWC.js
+++ CWC_project/client/src/components/CWC.js
@@ -7,7 +7,7 @@ function CWC() {
              
       <div className="cwc-text">Hello World,</div>
       <div className="cwc-paragrph">
-        CWC is an incentive token-based community. Web2.0 is the second stage of development of the World Wide Web, characterized especially by the change from static web pages to dynamic or user-generated content and the growth of social media.
+        CWC is an incentive token-based community. Web2.0 is the second stage of development of the World Wide Web, characterized especially by the change from static web pages to dynamic or user-generated content and the growth of social media. Hi
       </div>    
     </div>  
  
