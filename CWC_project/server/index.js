cb66cfe4adca13f7962ca90bc139fc2f6653b583
yonacode <62411918+yonacode@users.noreply.github.com>
2022-03-04 16:50:03 +0900

    mainpage/login/signup/logout
    mainpage/login/signup/logout

--- CWC_project/server/index.js
+++ CWC_project/server/index.js
@@ -1,46 +1,43 @@
-import dotenv from "dotenv";
-import express from "express";
-import mysql from "mysql";
-import cors from "cors";
-
-dotenv.config();
-
-const password = process.env.DATABASE_SPRINT_PASSWORD;
-
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const { urlencoded } = require("body-parser");
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
 const PORT = process.env.port || 8000;
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: password,
-  database: "cwcboard",
+    host: "localhost",
+    user: "root",
+    password: "fun",
+    database: "cwcboard"
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 //update board taking data from a database
-app.get("/api/get", (req, res) => {
-  const sqlQuery = "SELECT * FROM cwcboard;";
-  db.query(sqlQuery, (err, result) => {
-    res.send(result);
-  });
-});
+app.get("/api/get", (req, res)=>{
+    const sqlQuery = "SELECT * FROM cwcboard;";
+    db.query(sqlQuery, (err, result)=>{
+        res.send(result);
+    })
+})
+
 
 //store all posts on a database
 app.post("/api/insert", (req, res) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  // const user = req.body.username;
-  const sqlQuery = "INSERT INTO cwcboard (title, content) VALUES (?,?)";
-  db.query(sqlQuery, [title, content], (err, result) => {
-    res.send("success!");
-  });
+    const title = req.body.title;
+    const content = req.body.content;
+    // const user = req.body.username;
+    const sqlQuery = "INSERT INTO cwcboard (title, content) VALUES (?,?)";
+    db.query(sqlQuery, [title, content], (err, result) => {
+        res.send('success!');
+    });
 });
+
 
 // app.get("/", (req, res)=>{
 //     const sqlQuery = "INSERT INTO cwcboard (title, content) VALUES ('hello cwc', 'first post')";
@@ -49,6 +46,6 @@ app.post("/api/insert", (req, res) => {
 //     });
 // })
 
-app.listen(PORT, () => {
-  console.log(`running on port ${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`running on port ${PORT}`);
 });
