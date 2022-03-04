3b1d00308ba77a1e4055dac6fb822ffb6751010a
yonacode <62411918+yonacode@users.noreply.github.com>
2022-03-04 19:42:58 +0900

    Homepage & Board Database/Server
    Homepage & Board Database/Server

--- CWC_project/server/index.js
+++ CWC_project/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const mysql = require("mysql");
+const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser")
 // const { urlencoded } = require("body-parser");
@@ -116,6 +116,43 @@ app.get('/api/users/logout', auth, (req, res) => {
         })
     });
 
+
+// POSTING DATABASE // 
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "fun",
+    database: "cwcboard",
+  });
+
+//update board taking data from a database
+app.get("/api/get", (req, res)=>{
+    const sqlQuery = "SELECT * FROM cwcboard;";
+    db.query(sqlQuery, (err, result)=>{
+        res.send(result);
+    })
+})
+
+
+//store all posts on a database
+app.post("/api/insert", (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // const user = req.body.username;
+    const sqlQuery = "INSERT INTO cwcboard (title, content) VALUES (?,?)";
+    db.query(sqlQuery, [title, content], (err, result) => {
+        res.send('success!');
+    });
+});
+
+
+// app.get("/", (req, res)=>{
+//     const sqlQuery = "INSERT INTO cwcboard (title, content) VALUES ('hello cwc', 'first post')";
+//     db.query(sqlQuery, (err, result)=>{
+//         res.send('success!');
+//     });
+// })
 
 
 const PORT = process.env.port || 8000;
